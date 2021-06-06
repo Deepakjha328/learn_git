0cff4bcdf4e850ec7603e5f919246bec7b25cd23
Deepakjha328 <deepakkumar86561@gmail.com>
2021-06-06 19:10:55 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Multilevel inheritance-when more than one parent class inherited by child called then it is called multilev
+
+
+s= "deepakkumar"
+
+
+s2=s.find('pak')
+print(s2)
+l = [1,2,3,4,5,6,7]
+l1 = l[:-1]
+print(l1)
+
+
